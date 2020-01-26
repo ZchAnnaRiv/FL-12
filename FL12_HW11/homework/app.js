8d721f7581ba9852e1c4a8f9ac71febc44d6642a
Anna <channa.riv@gmail.com>
2020-01-26 20:51:12 +0200

    fixed bugs with folders loading

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -42,27 +42,33 @@ function setElementStyle(folder) {
     styleElement.classNameLI = folder ? 'folder' : 'just-file';
     styleElement.classNameI = folder ? 'folder-icon' : 'file-icon';
     styleElement.nameI = folder ? 'folder' : 'insert_drive_file';
+
     return styleElement;
 }
 
-function addNewListElement(data) {
+function addNewListElement(treeElements) {
     let newLI = document.createElement('li');
     let newDiv = document.createElement('div');
-    let styleElement = setElementStyle(data.folder);
+    let styleElement = setElementStyle(treeElements.folder);
     newDiv.innerHTML += '<i class= "material-icons ' + styleElement.classNameI + '">' + styleElement.nameI + '</i>';
-    newDiv.innerHTML += `${data.title}`;
-    if (data.folder) {
-        newDiv.setAttribute('class', `div-folder`);
-    }
-    newLI.setAttribute('class', `${styleElement.classNameLI}`);
-    newLI.setAttribute('id', `ul${data.title}`);
+    newDiv.innerHTML += `${treeElements.title}`;
 
+    if (treeElements.folder) {
+        newDiv.setAttribute('class', `div-folder`);
+        changeFolderStatusOnClick(newDiv, treeElements);
+    }
+
+    newLI.setAttribute('class', `${styleElement.classNameLI}`);
+    newLI.setAttribute('id', `ul${treeElements.title}`);
     newLI.appendChild(newDiv);
-    if (data.folder && !data.children) {
+
+    if (treeElements.folder && !treeElements.children) {
         let p = document.createElement('p');
         p.innerHTML = 'Folder is empty';
+        p.setAttribute('class', 'display-none');
         newLI.append(p);
     }
+
     return newLI;
 }
 
@@ -74,42 +80,26 @@ function buildFolderStructure(data, id) {
         let childLI = addNewListElement(data[i]);
         newUL.appendChild(childLI);
     }
+
     currentRoot.appendChild(newUL);
-
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].children) {
-            buildFolderStructure(data[i].children, `ul${data[i].title}`);
-        }
-    }
-
-
 }
 
 buildFolderStructure(structure, 'root');
 
-let folders = document.getElementsByClassName('div-folder');
+function changeFolderStatusOnClick(el, data) {
+    el.addEventListener('click', function () {
 
-function changeFolderStatusOnClick(folders) {
-    for (let el of folders) {
-        el.addEventListener('click', function () {
-            if (el.getElementsByClassName('folder-icon')[0].innerText === 'folder') {
-                el.getElementsByClassName('folder-icon')[0].innerText = 'folder_open';
-                el.nextSibling.setAttribute('class', 'display-block');
-            } else {
-                el.getElementsByClassName('folder-icon')[0].innerText = 'folder';
-                el.nextSibling.setAttribute('class', 'display-none');
-            }
-        })
-    }
+        if (data.children) {
+            buildFolderStructure(data.children, `ul${data.title}`);
+            data = data.children;
+        }
+
+        if (el.getElementsByClassName('folder-icon')[0].innerText === 'folder') {
+            el.getElementsByClassName('folder-icon')[0].innerText = 'folder_open';
+            el.nextSibling.setAttribute('class', 'display-block');
+        } else {
+            el.getElementsByClassName('folder-icon')[0].innerText = 'folder';
+            el.nextSibling.setAttribute('class', 'display-none');
+        }
+    })
 }
-
-changeFolderStatusOnClick(folders);
-
-for (let el of folders) {
-    const TWO = 2;
-    for (let i = 0; i < TWO; i++) {
-        el.click();
-    }
-}
-
-
